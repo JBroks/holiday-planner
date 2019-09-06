993525fe241fb3090a81ad57f350ee72e0c03222
Joanna Broks <asia.ostr@gmail.com>
2019-09-06 11:35:20 +0000

    Map style changed to color and autocomplete (city) based on selected country feature added

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,179 +1,123 @@
+var google;
+
+var countryRestrict;
+var autocomplete;
+var map, places;
+var countries = {
+    'all': {
+        center: { lat: 45.4, lng: 0 },
+        zoom: 2.9
+    },
+    'au': {
+        center: { lat: -25.3, lng: 133.8 },
+        zoom: 4
+    },
+    'br': {
+        center: { lat: -14.2, lng: -51.9 },
+        zoom: 4
+    },
+    'ca': {
+        center: { lat: 62, lng: -110.0 },
+        zoom: 3.5
+    },
+    'eg': {
+        center: { lat: 26.2, lng: 30.3 },
+        zoom: 6
+    },
+    'gr': {
+        center: { lat: 39.2, lng: 22.5 },
+        zoom: 6.5
+    },
+    'mx': {
+        center: { lat: 23.6, lng: -102.5 },
+        zoom: 5
+    },
+    'nz': {
+        center: { lat: -40.9, lng: 174.9 },
+        zoom: 5.5
+    },
+    'it': {
+        center: { lat: 41.9, lng: 12.6 },
+        zoom: 6
+    },
+    'za': {
+        center: { lat: -30.6, lng: 22.9 },
+        zoom: 5
+    },
+    'es': {
+        center: { lat: 40.5, lng: -3.7 },
+        zoom: 6
+    },
+    'tr': {
+        center: { lat: 39.2, lng: 35.5 },
+        zoom: 6
+    },
+    'us': {
+        center: { lat: 37.1, lng: -95.7 },
+        zoom: 4.5
+    }
+};
+
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 45.34512, lng: 0 },
-        zoom: 2.9,
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: countries['all'].zoom,
+        center: countries['all'].center,
+        mapTypeControl: false,
+        panControl: false,
+        zoomControl: false,
+        streetViewControl: false,
         mapTypeId: 'roadmap',
         styles: [{
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#f5f5f5"
-                }]
-            },
-            {
-                "elementType": "labels.icon",
-                "stylers": [{
-                    "visibility": "off"
-                }]
-            },
-            {
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#616161"
-                }]
-            },
-            {
-                "elementType": "labels.text.stroke",
-                "stylers": [{
-                    "color": "#f5f5f5"
-                }]
-            },
-            {
-                "featureType": "administrative.land_parcel",
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#bdbdbd"
-                }]
-            },
-            {
-                "featureType": "poi",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#eeeeee"
-                }]
-            },
-            {
-                "featureType": "poi",
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#757575"
-                }]
-            },
-            {
-                "featureType": "poi.park",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#e5e5e5"
-                }]
-            },
-            {
-                "featureType": "poi.park",
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#9e9e9e"
-                }]
-            },
-            {
-                "featureType": "road",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#ffffff"
-                }]
-            },
-            {
-                "featureType": "road.arterial",
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#757575"
-                }]
-            },
-            {
                 "featureType": "road.highway",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#dadada"
-                }]
-            },
-            {
-                "featureType": "road.highway",
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#616161"
-                }]
-            },
-            {
-                "featureType": "road.local",
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#9e9e9e"
-                }]
-            },
-            {
-                "featureType": "transit.line",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#e5e5e5"
-                }]
-            },
-            {
-                "featureType": "transit.station",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#eeeeee"
-                }]
-            },
-            {
-                "featureType": "water",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#c9c9c9"
-                }]
-            },
-            {
-                "featureType": "water",
                 "elementType": "geometry.fill",
                 "stylers": [{
-                    "color": "#6b6e70"
+                    "color": "#fbeec1"
                 }]
             },
             {
                 "featureType": "water",
-                "elementType": "labels.text.fill",
+                "elementType": "geometry",
                 "stylers": [{
-                    "color": "#9e9e9e"
+                    "color": "#659dbd"
                 }]
             }
         ]
+
     });
 
-    //Create the search box and link it to the UI element.
-    var input = document.getElementById('pac-input');
-    var options = { types: 'cities'};
-    var searchBox = new google.maps.places.SearchBox(input, options);
+    // Add a DOM event listener to react when the user selects a country.
+    document.getElementById('country').addEventListener(
+        'change', setAutocompleteCountry);
 
-
-    //Bias the SearchBox results towards current map's viewport.
-    map.addListener('bounds_changed', function() {
-        searchBox.setBounds(map.getBounds());
-    });
-
-
-    //Listen for the event fired when the user selects a prediction and retrieve
-    //more details for that place.
-
-
-    searchBox.addListener('places_changed', function() {
-        var places = searchBox.getPlaces();
-
-        if (places.length == 0) {
-            return;
-        }
-
-        // For each place, get the location.
-        var bounds = new google.maps.LatLngBounds();
-        places.forEach(function(place) {
-            if (!place.geometry) {
-                console.log("Returned place contains no geometry");
-                return;
-            }
-
-            if (place.geometry.viewport) {
-                // Only geocodes have viewport.
-                bounds.union(place.geometry.viewport);
-            }
-            else {
-                bounds.extend(place.geometry.location);
-            }
+    // Create the autocomplete object and associate it with the UI input control.
+    // Restrict the search to the default country, and to place type "cities".
+    autocomplete = new google.maps.places.Autocomplete(
+        (document.getElementById('city-input')), {
+            types: ['(cities)'],
+            componentRestrictions: countryRestrict
         });
-        map.fitBounds(bounds);
-    });
+
+    autocomplete.addListener('place_changed', onPlaceChanged);
+
+    // Set the country restriction based on user input.
+    // Also center and zoom the map on the given country.
+    function setAutocompleteCountry() {
+
+        var country = document.getElementById('country').value;
+        autocomplete.setComponentRestrictions({ 'country': country });
+        map.setCenter(countries[country].center);
+        map.setZoom(countries[country].zoom);
+
+    }
+
+    // When the user selects a city, get the place details for the city and
+    // zoom the map in on the city.
+    function onPlaceChanged() {
+        var place = autocomplete.getPlace();
+        if (place.geometry) {
+            map.panTo(place.geometry.location);
+            map.setZoom(15);
+        }
+    }
+
 }
